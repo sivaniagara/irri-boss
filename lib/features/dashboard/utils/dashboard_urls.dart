559f5e1496a10a12fb90c427154dd7f83d742a72
N.S.Venkatesasn <venkatesan.niagara@gmail.com>
2025-12-31 12:56:20 +0530

    dashborad

--- lib/features/dashboard/utils/dashboard_urls.dart
+++ lib/features/dashboard/utils/dashboard_urls.dart
@@ -1,5 +1,21 @@
+
 class DashboardUrls {
   /// Get methods
   static const String dashboardForGroupUrl = 'controller/user/:userId/cluster';
   static const String dashboardUrl = 'user/:userId/cluster/:groupId/controller';
+  static const String motorOnOffUrl = 'user/:userId/subuser/:subuserId/controller/:controllerId/manualstatus';
+
+  static String get todayFormatted => Conversions.dateFormatter.format(DateTime.now());
+
+  static String nodeStatusUrl({
+    required String userId,
+    required String subuserId,
+    required String controllerId,
+  }) {
+    final String fromDate = todayFormatted;
+    final String toDate = todayFormatted;
+
+    return 'user/$userId/subuser/$subuserId/controller/$controllerId/report'
+        '?fromDate=$fromDate&toDate=$toDate&type=mappednodes';
+  }
 }
