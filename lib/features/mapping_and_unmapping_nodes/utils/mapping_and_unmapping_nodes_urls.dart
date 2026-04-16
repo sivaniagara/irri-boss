189b86ec1b4aa7e29cf753d260f41b3bc81dd759
Siva Prakash <sivaprakash.niagara@gmail.com>
2026-04-16 09:42:59 +0530

    implement new unmap url

--- lib/features/mapping_and_unmapping_nodes/utils/mapping_and_unmapping_nodes_urls.dart
+++ lib/features/mapping_and_unmapping_nodes/utils/mapping_and_unmapping_nodes_urls.dart
@@ -1,7 +1,6 @@
 class MappingAndUnmappingNodesUrls{
   static String getMappingAndUnmappingNode = 'user/:userId/controller/:controllerId/nodeUnMapList';
-  static String deleteMappedNode = "user/:userId/controller/:controllerId/node/:nodeId?sentSms=':payload'";
+  static String deleteMappedNode = "user/:userId/controller/:controllerId/node/:nodeId/remove?sentSms=':payload'";
   static String unMappedToMapped = "user/:userId/controller/:controllerId/node";
   static const String sentAndReceive = "user/:userId/controller/:controllerId/program/:programId/timeflowsetting/sendAndReceived";
-
 }
