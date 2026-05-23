ff460d22b1d023abc3e38f472c6bb0f05e0283da
rahul <rahulniagaradev2025@gmail.com>
2026-05-23 12:56:32 +0530

    payload correction

--- lib/features/mapping_and_unmapping_nodes/data/models/unmapped_category_node_model.dart
+++ lib/features/mapping_and_unmapping_nodes/data/models/unmapped_category_node_model.dart
@@ -39,7 +39,7 @@ class UnmappedCategoryNodeModel extends UnmappedCategoryNodeEntity {
   Map<String, dynamic> formPayload(String categoryId, String serialNo){
     return {
       'nodeId' : nodeId,
-      'sentSms' : 'SlSET$serialNo,${splitQrCode(qrCode)}',
+      'sentSms' : 'SLSET$serialNo,${splitQrCode(qrCode)}',
       'categoryId' : categoryId
     };
   }
